d59c9453cd8b188ace2d4cc4777dcc2ae6a697eb
Alexey Alekhin <laughedelic@gmail.com>
2013-09-20 19:08:11 +0200

    Updated to sbt-statika v0.9 (statika v0.15)

--- build.sbt
+++ build.sbt
@@ -4,8 +4,15 @@ description := "A bundle for s3cmd tool"
 
 homepage := Some(url("https://github.com/ohnosequences/s3cmd"))
 
+organization := "ohnosequences"
+
+organizationHomepage := Some(url("http://ohnosequences.com"))
+
 licenses := Seq("AGPLv3" -> url("http://www.gnu.org/licenses/agpl-3.0.txt"))
 
 bundleObjects := Seq("ohnosequences.statika.S3cmd")
 
-libraryDependencies ++= Seq( "ohnosequences" %% "git" % "0.6.0" ,  "ohnosequences" %% "python" % "0.1.0" ) 
+libraryDependencies ++= Seq( 
+  "ohnosequences" %% "git" % "0.7.0-SNAPSHOT"
+, "ohnosequences" %% "python" % "0.2.0-SNAPSHOT"
+) 
--- src/main/scala/S3cmd.scala
+++ src/main/scala/S3cmd.scala
@@ -7,25 +7,12 @@ case object S3cmd extends Bundle(Git :: Python :: HNil) {
 
   val metadata = generated.metadata.S3cmd
 
-  // Adding method to run commands from a given path
-  implicit class PBAt(cmd: Seq[String]) {
-    implicit def @@(path: String): ProcessBuilder =
-      Process(cmd, new java.io.File(path), "" -> "")
-  }
+  val s3cmd = new java.io.File("s3cmd")
 
-  def install[D <: DistributionAux](distribution: D): InstallResults = {
-
-    val trace = List[ProcessBuilder](
-      Seq("git", "clone", "https://github.com/s3tools/s3cmd.git")
-    , Seq("python", "setup.py", "install") @@ "s3cmd"
-    ).foldLeft(List[InstallResult]()){ (acc, cmd) => 
-      if (acc exists (_.isLeft)) acc
-      else if (cmd.! != 0) acc ::: failure(cmd.toString)
-      else acc ::: success(cmd.toString)
-    }
-
-    if (trace exists (_.isLeft)) trace
-    else success("%s is installed" format metadata)
-  }
+  def install[D <: AnyDistribution](distribution: D): InstallResults =
+     (  Git.clone("https://github.com/s3tools/s3cmd.git")
+    -&- "python setup.py install" @@ s3cmd
+    ->- success(metadata+" is installed")
+     )
 
 }
