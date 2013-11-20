e70f048f43b1ba2827f4e1899a655bc78073096a
Alexey Alekhin <laughedelic@gmail.com>
2013-11-20 12:30:08 +0100

    Upgraded to statika-1.0

---
+++ info.sbt
@@ -0,0 +1,11 @@
+Statika.bundleProject
+
+name := "s3cmd"
+
+description := "A bundle for s3cmd tool"
+
+organization := "ohnosequences"
+
+libraryDependencies += "ohnosequences" %% "yum" % "0.2.0"
+
+generateDocs := {}
--- src/main/scala/S3cmd.scala
+++ src/main/scala/S3cmd.scala
@@ -1,13 +1,13 @@
-package ohnosequences.statika
+package ohnosequences.statika.bundles
 
 import ohnosequences.statika._
 import sys.process._
 
 case object Python extends YumBundle("python")
 
-case object S3cmd extends Bundle(Git :+: Python) {
+case object S3cmd extends Bundle(Git :~: Python) {
 
-  def install[D <: AnyDistribution](distribution: D): InstallResults =
+  override def install[D <: AnyDistribution](d: D): InstallResults =
      (  Git.clone("https://github.com/s3tools/s3cmd.git")
     -&- "python setup.py install" @@ (new java.io.File("s3cmd"))
     ->- success(name+" is installed")
